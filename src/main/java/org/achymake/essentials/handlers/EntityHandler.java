a8b5807ac52c99172a2c841496681b0ed1bb692a
AchyMake <achymake@gmail.com>
2024-11-10 06:58:14 +0100

    patched spawn reason

--- src/main/java/org/achymake/essentials/handlers/EntityHandler.java
+++ src/main/java/org/achymake/essentials/handlers/EntityHandler.java
@@ -3,13 +3,9 @@ package org.achymake.essentials.handlers;
 import org.achymake.essentials.Essentials;
 import org.achymake.essentials.data.Entities;
 import org.bukkit.block.Block;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-
-import java.io.File;
 
 public record EntityHandler(Entity getEntity) {
     private Essentials getInstance() {
@@ -21,41 +17,38 @@ public record EntityHandler(Entity getEntity) {
     public boolean exists() {
         return getEntities().exists(getType());
     }
-    public FileConfiguration getConfig() {
-        return getEntities().getConfig(getType());
-    }
     public boolean isHostile() {
-        return getConfig().getBoolean("hostile");
+        return getEntities().isHostile(getType());
     }
     public boolean isFriendly() {
         return !isHostile();
     }
     public int chunkLimit() {
-        return getConfig().getInt("chunk-limit");
+        return getEntities().chunkLimit(getType());
     }
     public boolean disableSpawn() {
-        return getConfig().getBoolean("disable-spawn");
+        return getEntities().disableSpawn(getType());
     }
     public boolean disableBlockForm() {
-        return getConfig().getBoolean("disable-block-form");
+        return getEntities().disableBlockForm(getType());
     }
     public boolean disableBlockDamage() {
-        return getConfig().getBoolean("disable-block-damage");
+        return getEntities().disableBlockDamage(getType());
     }
     public boolean disableBlockChange() {
-        return getConfig().getBoolean("disable-block-change");
+        return getEntities().disableBlockChange(getType());
     }
     public boolean disableBlockInteract(Block block) {
-        return getConfig().getBoolean("disable-block-interact." + block.getType());
+        return getEntities().disableBlockInteract(getType(), block.getType());
     }
     public boolean disableTarget(Entity entity) {
-        return getConfig().getBoolean("disable-target." + entity.getType());
+        return getEntities().disableTarget(getType(), entity.getType());
     }
     public boolean disableDamage(Entity entity) {
-        return getConfig().getBoolean("disable-damage." + entity.getType());
+        return getEntities().disableDamage(getType(), entity.getType());
     }
     public boolean disableSpawnReason(CreatureSpawnEvent.SpawnReason spawnReason) {
-        return getConfig().getBoolean("disable-spawn-reason." + spawnReason.toString());
+        return getEntities().disabledSpawnReason(getType(), spawnReason);
     }
     public String getName() {
         if (getEntity().getType().equals(EntityType.PLAYER)) {
