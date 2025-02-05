4381a06e37583ae62885fbbc2655094a6c16e8ea
AchyMake <achymake@gmail.com>
2025-02-05 14:57:11 +0100

    patched PrepareAnvil

--- src/main/java/org/achymake/essentials/listeners/PrepareAnvil.java
+++ src/main/java/org/achymake/essentials/listeners/PrepareAnvil.java
@@ -2,7 +2,6 @@ package org.achymake.essentials.listeners;
 
 import org.achymake.essentials.Essentials;
 import org.achymake.essentials.data.Message;
-import org.achymake.essentials.handlers.MaterialHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,9 +11,6 @@ import org.bukkit.plugin.PluginManager;
 public class PrepareAnvil implements Listener {
     private Essentials getInstance() {
         return Essentials.getInstance();
-    }
-    private MaterialHandler getMaterials() {
-        return getInstance().getMaterialHandler();
     }
     private Message getMessage() {
         return getInstance().getMessage();
@@ -33,28 +29,10 @@ public class PrepareAnvil implements Listener {
         var view = event.getView();
         var player = view.getPlayer();
         var rename = view.getRenameText();
-        if (rename != null) {
-            if (rename.contains("&") && player.hasPermission("essentials.event.anvil.color")) {
-                meta.setDisplayName(getMessage().addColor(rename));
-            }
-        }
-        var secondItem = event.getInventory().getSecondItem();
-        if (secondItem != null && secondItem.getType().equals(getMaterials().get("enchanted_book"))) {
-            var secondItemMeta = secondItem.getItemMeta();
-            if (secondItemMeta.hasEnchants()) {
-                secondItemMeta.getEnchants().keySet().forEach(enchantment -> {
-                    var bookLevel = secondItemMeta.getEnchants().get(enchantment);
-                    var maxLevel = enchantment.getMaxLevel();
-                    if (bookLevel > maxLevel) {
-                        if (player.hasPermission("essentials.event.anvil.unsafe")) {
-                            meta.addEnchant(enchantment, bookLevel, true);
-                        } else if (player.hasPermission("essentials.event.anvil.safe")) {
-                            meta.addEnchant(enchantment, maxLevel, true);
-                        }
-                    }
-                });
-            }
-        }
+        if (rename == null)return;
+        if (!rename.contains("&"))return;
+        if (!player.hasPermission("essentials.event.anvil.color"))return;
+        meta.setDisplayName(getMessage().addColor(rename));
         result.setItemMeta(meta);
         event.setResult(result);
     }
